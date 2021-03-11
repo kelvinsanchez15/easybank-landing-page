92079325042c42b6735c1a17b919fac08119a3a5
Kelvin Sanchez <kelvinsanchez15@gmail.com>
2021-03-11 15:00:39 -0400

    improve responsiveness

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -1,14 +1,11 @@
 export default function Hero() {
   return (
-    <section id="hero" className="relative overflow-hidden">
-      <div className="w-full h-full lg:h-full absolute top-0 lg:-right-42.6%">
-        <div className="bg-header-mobile bg-custom-mobile-header-size absolute top-0 w-full h-full bg-no-repeat lg:hidden"></div>
-        <div className="bg-header-desktop absolute -top-8 w-full h-full bg-no-repeat bg-left-bottom hidden lg:block"></div>
-        <div className="bg-image-mockups bg-custom-mobile-mockup-size lg:bg-auto absolute -top-14 bg-top lg:top-0 lg:-right-40 w-full h-full bg-no-repeat lg:bg-left-bottom"></div>
-      </div>
-      <div className="container h-screen">
+    <section id="hero" className="relative">
+      <div className="bg-header-mobile bg-custom-mobile-header-size absolute w-full h-full bg-no-repeat lg:hidden"></div>
+      <div className="bg-header-desktop absolute w-full h-full bg-no-repeat hidden lg:block bg-left -right-42.6%"></div>
+      <div className="container h-screen relative z-20">
         <div className="h-full flex flex-col justify-end text-center items-center pb-4 lg:pb-0 lg:w-96 lg:text-left lg:justify-center lg:items-start">
-          <h1 className="font-light text-4xl lg:text-5xl text-primary-dark-blue pb-5">
+          <h1 className="text-4xl lg:text-5xl text-primary-dark-blue pb-5">
             Next generation digital banking
           </h1>
           <p className="text-neutral-grayish-blue text-xs lg:text-base leading-5 mb-7">
--- pages/index.tsx
+++ pages/index.tsx
@@ -13,10 +13,14 @@ const Home: NextPage = () => {
         <title>Easybank</title>
         <link rel="icon" href="/favicon-32x32.png" />
       </Head>
-
       <Navbar />
-      <Hero />
-      <Motivation />
+      <div className="relative overflow-hidden">
+        <div className="w-full h-full absolute">
+          <div className="bg-image-mockups absolute z-20 w-full h-full bg-no-repeat bg-custom-mobile-mockup-size bg-top lg:bg-auto lg:bg-right-top lg:-right-72 xl:-right-28"></div>
+        </div>
+        <Hero />
+        <Motivation />
+      </div>
       <Blog />
       <Footer />
     </>
