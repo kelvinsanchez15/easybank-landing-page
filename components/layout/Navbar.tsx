7085c4284e6b8394313be8b564dfb555127c455e
Kelvin Sanchez <kelvinsanchez15@gmail.com>
2021-03-12 15:38:23 -0400

    add modal functionality to navbar

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -1,37 +1,57 @@
+import { useState } from 'react';
+
+const navItems = ['Home', 'About', 'Contact', 'Blog', 'Careers'];
+
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <div className="fixed z-50 top-0 w-full bg-white">
-      <nav className="container flex justify-between items-center z-20">
-        <div className="my-5 lg:my-6">
-          <img src="/images/logo.svg" alt="easybank logo" />
+    <>
+      <div className="fixed z-50 top-0 w-full bg-white">
+        <nav className="container flex justify-between items-center z-20">
+          <div className="my-5 lg:my-6">
+            <img src="/images/logo.svg" alt="easybank logo" />
+          </div>
+
+          <div className="hidden lg:block text-sm text-neutral-grayish-blue">
+            {navItems.map((navItem) => (
+              <a className="mx-3 py-5 hover:gradient-border-bottom" href="#">
+                {navItem}
+              </a>
+            ))}
+          </div>
+
+          <button className="hidden lg:block bg-primary-lime-green px-7 py-3 rounded-full text-neutral-white text-xs bg-gradient-to-r from-primary-lime-green to-primary-bright-cyan hover:button-brightness">
+            Request Invite
+          </button>
+
+          <button onClick={() => setIsOpen(!isOpen)} className="lg:hidden">
+            <img
+              className={`${isOpen && 'hidden'}`}
+              src="/icons/icon-hamburger.svg"
+              alt=""
+            />
+            <img
+              className={isOpen ? 'block' : 'hidden'}
+              src="/icons/icon-close.svg"
+              alt=""
+            />
+          </button>
+        </nav>
+      </div>
+
+      {/* Modal */}
+      <div
+        className={`fixed inset-0 z-30 bg-gray-800 
+      bg-opacity-50 ${isOpen ? 'block' : 'hidden'}`}
+      >
+        <div className="bg-white text-primary-dark-blue flex flex-col text-center mx-5 my-20 py-4 rounded">
+          {navItems.map((navItem) => (
+            <a className="py-2" href="#">
+              {navItem}
+            </a>
+          ))}
         </div>
-
-        <div className="hidden lg:block text-sm text-neutral-grayish-blue">
-          <a className="mx-3 py-5 hover:gradient-border-bottom" href="#">
-            Home
-          </a>
-          <a className="mx-3 py-5 hover:gradient-border-bottom" href="#">
-            About
-          </a>
-          <a className="mx-3 py-5 hover:gradient-border-bottom" href="#">
-            Contact
-          </a>
-          <a className="mx-3 py-5 hover:gradient-border-bottom" href="#">
-            Blog
-          </a>
-          <a className="mx-3 py-5 hover:gradient-border-bottom" href="#">
-            Careers
-          </a>
-        </div>
-
-        <button className="hidden lg:block bg-primary-lime-green px-7 py-3 rounded-full text-neutral-white text-xs bg-gradient-to-r from-primary-lime-green to-primary-bright-cyan hover:button-brightness">
-          Request Invite
-        </button>
-
-        <button className="lg:hidden">
-          <img src="/icons/icon-hamburger.svg" alt="" />
-        </button>
-      </nav>
-    </div>
+      </div>
+    </>
   );
 }
